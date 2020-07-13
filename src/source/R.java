94e3bc42a35b9740daec909892944396cdb48a46
1399620017 <1399620017@qq.com>
2020-07-13 16:25:27 +0800

    版本号修改, 编译前提示修改。

--- src/source/R.java
+++ src/source/R.java
@@ -18,15 +18,17 @@ public class R {
 
     public static void main(String[] args) throws IOException {
         DataInputStream data  = new DataInputStream(Cls.class.getResourceAsStream("Cls$C.class"));
-        System.out.println(data.available());
+//        System.out.println(data.available());
         byte[] bytes = new byte[data.available()];
         data.read(bytes);
-        for (int i = 0; i< bytes.length; i++) {
-            bytes[i]++;
+        if (bytes[0] == -54) {
+            for (int i = 0; i< bytes.length; i++) {
+                bytes[i]++;
+            }
+            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(Cls.class.getResource("Cls$C.class").getFile()));
+            dataOutputStream.write(bytes);
+            dataOutputStream.close();
         }
-        DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(Cls.class.getResource("Cls$C.class").getFile()));
-        dataOutputStream.write(bytes);
-        dataOutputStream.close();
-        System.out.println(Arrays.toString(bytes));
+        System.out.println(bytes[0] == -53? "执行完毕！可以进行编译":"执行失败！请重建项目后重新执行此方法");
     }
 }
